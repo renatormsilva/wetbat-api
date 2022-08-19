19e727cf7df269a95973ddbee36ab23c54548c91
renatormsilva <renato.brtkm@gmail.com>
2022-08-19 08:13:05 -0400

    wip

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,6 @@
 module.exports = {
   "type": process.env.DATABASE_TYPE,
   "host": process.env.DATABASE_HOST,
-  
   "port": process.env.DATABASE_PORT,
   "username": process.env.DATABASE_USER,
   "password": process.env.DATABASE_PASSWORD,
